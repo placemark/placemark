863c6972dfcef6775b9bf2eb1d000986101bcbe8
Tom MacWright <tom@macwright.com>
2024-02-18 11:06:33 -0500

    Remove withSentry

--- pages/api/v1/healthcheck.ts
+++ pages/api/v1/healthcheck.ts
@@ -1,12 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { withSentry } from "@sentry/nextjs";
 import db from "db";
 
 /**
  * As the healthcheck for the api.placemark.io
  * subdomain
  */
-export default withSentry(async function healthcheck(
+export default async function healthcheck(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -20,4 +19,4 @@ export default withSentry(async function healthcheck(
     ok: true,
     sha: process.env.RENDER_GIT_COMMIT,
   });
-});
+}
