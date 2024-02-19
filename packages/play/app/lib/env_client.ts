5d07b09b113c9228250a6ab858ec9331e10193e6
Tom MacWright <tom@macwright.com>
2024-02-19 10:36:34 -0500

    .

--- packages/play/app/lib/env_client.ts
+++ packages/play/app/lib/env_client.ts
@@ -1,4 +1,4 @@
-import { envsafe, str, num } from "envsafe";
+import { envsafe, str } from "envsafe";
 
 export const env = envsafe({
   /**
@@ -16,48 +16,6 @@ export const env = envsafe({
    */
   NEXT_PUBLIC_GEOCODE_EARTH_TOKEN: str({
     input: process.env.NEXT_PUBLIC_GEOCODE_EARTH_TOKEN,
-    devDefault: "_",
-  }),
-  /**
-   * There's a separate host for websockets:
-   * this points to it.
-   */
-  NEXT_PUBLIC_WS_HOST: str({
-    input: process.env.NEXT_PUBLIC_WS_HOST,
-    devDefault: "ws.placemark.io",
-  }),
-  /**
-   * Limit for files at the frontend side.
-   */
-  NEXT_PUBLIC_FILE_LIMIT_MB: num({
-    input: process.env.NEXT_PUBLIC_FILE_LIMIT_MB,
-  }),
-  /**
-   * Warn people with files at least this big.
-   */
-  NEXT_PUBLIC_FILE_WARN_MB: num({
-    input: process.env.NEXT_PUBLIC_FILE_WARN_MB,
-  }),
-  /**
-   * Domain of the site when deployed.
-   */
-  NEXT_PUBLIC_DOMAIN_WITH_SLASH: str({
-    input: process.env.NEXT_PUBLIC_DOMAIN_WITH_SLASH,
-    devDefault: "http://localhost:3000/",
-  }),
-  /**
-   * Domain of the API when deployed
-   */
-  NEXT_PUBLIC_API_BASE_WITH_SLASH: str({
-    input: process.env.NEXT_PUBLIC_API_BASE_WITH_SLASH,
-    devDefault: "http://localhost:3000/",
-  }),
-  /**
-   * Key for authenticating with Replicache
-   * https://replicache.dev/
-   */
-  NEXT_PUBLIC_REPLICACHE_KEY: str({
-    input: process.env.NEXT_PUBLIC_REPLICACHE_KEY,
     devDefault: "_",
   }),
 });
