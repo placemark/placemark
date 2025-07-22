d9ab8ed933d92c44e819242ce4d3d724fd7d3161
Tom MacWright <tom@macwright.com>
2025-07-22 09:27:02 -0400

    Progress toward vite, replacing next/dynamic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,12 @@ import { Tooltip as T } from "radix-ui";
 import { QueryClientProvider, QueryClient } from "react-query";
 import "core-js/features/array/at";
 
-const RouterProgressBar = () =>
-	import("app/components/router_progress_bar").then((m) => m.RouterProgressBar);
-
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: any) {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<T.Provider>
-				<RouterProgressBar />
 				<Component {...pageProps} />
 			</T.Provider>
 		</QueryClientProvider>
