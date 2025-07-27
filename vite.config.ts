04bb230ff50aa1fb268238311bec70ecd5700207
Tom MacWright <tom@macwright.com>
2025-07-27 17:53:58 -0400

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	worker: {
+		format: "es",
+		plugins: () => [tsconfigPaths()],
+	},
 	test: {
 		dir: "./",
 		deps: {
