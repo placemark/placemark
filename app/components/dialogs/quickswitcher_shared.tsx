7b4b77dc75aef1b4d245c44c49cddc30c573d455
Tom MacWright <tom@macwright.com>
2025-09-21 13:07:06 -0400

    Fix TSC issues, fix Flatgeobuf import

--- app/components/dialogs/quickswitcher_shared.tsx
+++ app/components/dialogs/quickswitcher_shared.tsx
@@ -1,13 +1,13 @@
 import { QItem } from "app/lib/geocode";
-import Fuse from "fuse.js";
+import type { FuseResultMatch } from "fuse.js";
 import { truncate } from "app/lib/utils";
 import { Combobox } from "@headlessui/react";
 import { match } from "ts-pattern";
 import {
-  GlobeIcon,
-  GroupIcon,
-  MagnifyingGlassIcon,
-  SewingPinIcon,
+	GlobeIcon,
+	GroupIcon,
+	MagnifyingGlassIcon,
+	SewingPinIcon,
 } from "@radix-ui/react-icons";
 import { Folder16 } from "app/components/icons";
 
@@ -24,110 +24,110 @@ export const comboboxFooterClass = `bg-gray-200 dark:bg-gray-800
       text-gray-700 dark:text-gray-200 w-full`;
 
 function QItemDisplay({ item, active }: { item: QItem; active: boolean }) {
-  const content = match(item)
-    .with({ type: "Feature" }, (item) => (
-      <>
-        <MagnifyingGlassIcon className="opacity-30" />
-        <div className="truncate">{item.properties.label}</div>
-      </>
-    ))
-    .with({ type: "extent" }, (item) => (
-      <>
-        <GroupIcon className="opacity-30" />
-        Pan to {item.name}
-      </>
-    ))
-    .with({ type: "coordinate" }, (item) => (
-      <>
-        <SewingPinIcon className="opacity-30" />
-        Pan to {item.name}
-      </>
-    ))
-    .with({ type: "action" }, (item) => {
-      return (
-        <>
-          {item.action.icon}
-          <div className="truncate">{item.action.label}</div>
-        </>
-      );
-    })
-    .with({ type: "wrappedFeature" }, (item) => {
-      return (
-        <>
-          <SewingPinIcon className="opacity-30" />
-          <div className="truncate">
-            {item.result.item.feature.geometry?.type || ""} feature
-            {formatMatches(item.result.matches)}
-          </div>
-        </>
-      );
-    })
-    .with({ type: "leaf" }, (item) => {
-      return (
-        <>
-          <GlobeIcon className="opacity-30" />
-          {item.data.name}
-        </>
-      );
-    })
-    .with({ type: "container" }, (item) => (
-      <>
-        <Folder16 className="opacity-30" />
-        {item.data.name}
-      </>
-    ))
-    .exhaustive();
+	const content = match(item)
+		.with({ type: "Feature" }, (item) => (
+			<>
+				<MagnifyingGlassIcon className="opacity-30" />
+				<div className="truncate">{item.properties.label}</div>
+			</>
+		))
+		.with({ type: "extent" }, (item) => (
+			<>
+				<GroupIcon className="opacity-30" />
+				Pan to {item.name}
+			</>
+		))
+		.with({ type: "coordinate" }, (item) => (
+			<>
+				<SewingPinIcon className="opacity-30" />
+				Pan to {item.name}
+			</>
+		))
+		.with({ type: "action" }, (item) => {
+			return (
+				<>
+					{item.action.icon}
+					<div className="truncate">{item.action.label}</div>
+				</>
+			);
+		})
+		.with({ type: "wrappedFeature" }, (item) => {
+			return (
+				<>
+					<SewingPinIcon className="opacity-30" />
+					<div className="truncate">
+						{item.result.item.feature.geometry?.type || ""} feature
+						{formatMatches(item.result.matches)}
+					</div>
+				</>
+			);
+		})
+		.with({ type: "leaf" }, (item) => {
+			return (
+				<>
+					<GlobeIcon className="opacity-30" />
+					{item.data.name}
+				</>
+			);
+		})
+		.with({ type: "container" }, (item) => (
+			<>
+				<Folder16 className="opacity-30" />
+				{item.data.name}
+			</>
+		))
+		.exhaustive();
 
-  return (
-    <div
-      className={`
+	return (
+		<div
+			className={`
                 px-3 py-2
                 text-sm text-left
                 dark:text-gray-50
                 cursor-pointer
                 grid gap-x-1 items-center
                 text-black dark:text-white ${
-                  active
-                    ? "bg-purple-200 dark:bg-purple-400 dark:text-black"
-                    : "hover:bg-purple-200 dark:hover:bg-purple-400 dark:hover:text-black"
-                }`}
-      style={{
-        gridTemplateColumns: "1rem auto",
-      }}
-    >
-      {content}
-    </div>
-  );
+									active
+										? "bg-purple-200 dark:bg-purple-400 dark:text-black"
+										: "hover:bg-purple-200 dark:hover:bg-purple-400 dark:hover:text-black"
+								}`}
+			style={{
+				gridTemplateColumns: "1rem auto",
+			}}
+		>
+			{content}
+		</div>
+	);
 }
 
 export function ResultsOptions({
-  results,
-  title,
+	results,
+	title,
 }: {
-  results: QItem[];
-  title: string;
+	results: QItem[];
+	title: string;
 }) {
-  if (!results.length) return null;
+	if (!results.length) return null;
 
-  return (
-    <>
-      <div
-        style={{
-          fontSize: 10,
-        }}
-        className="uppercase text-gray-500 pl-4 pt-2"
-      >
-        {title}
-      </div>
-      {results.map((item, i) => (
-        <Combobox.Option value={item} key={i}>
-          {({ active }) => {
-            return <QItemDisplay item={item} active={active} />;
-          }}
-        </Combobox.Option>
-      ))}
-    </>
-  );
+	return (
+		<>
+			<div
+				style={{
+					fontSize: 10,
+				}}
+				className="uppercase text-gray-500 pl-4 pt-2"
+			>
+				{title}
+			</div>
+			{results.map((item, i) => (
+				<Combobox.Option value={item} key={i}>
+					{({ active }) => {
+						return <QItemDisplay item={item} active={active} />;
+					}}
+				</Combobox.Option>
+			))}
+		</>
+	);
 }
 
 /**
@@ -135,18 +135,16 @@ export function ResultsOptions({
  * is the match.
  * If we have one, format it. Otherwise null.
  */
-function formatMatches(
-  matches: readonly Fuse.FuseResultMatch[] | undefined
-) {
-  if (!matches?.length) return null;
-  const match = matches[0];
-  return (
-    <span>
-      {" "}
-      with{" "}
-      <span className="font-mono">
-        {truncate(match.key || "", 64)}: {truncate(match.value || "", 64)}
-      </span>
-    </span>
-  );
+function formatMatches(matches: readonly FuseResultMatch[] | undefined) {
+	if (!matches?.length) return null;
+	const match = matches[0];
+	return (
+		<span>
+			{" "}
+			with{" "}
+			<span className="font-mono">
+				{truncate(match.key || "", 64)}: {truncate(match.value || "", 64)}
+			</span>
+		</span>
+	);
 }
--- app/lib/convert/gpx.ts
+++ app/lib/convert/gpx.ts
@@ -6,28 +6,28 @@ import type { ConvertError } from "app/lib/errors";
 import { FeatureCollection } from "types";
 
 class CGPX implements FileType {
-  id = "gpx" as const;
-  label = "GPX";
-  extensions = [".gpx"];
-  mimes = [] as string[];
-  filenames = [] as string[];
-  forwardBinary(file: ArrayBuffer, _options: ImportOptions) {
-    return readAsText(file).chain((text) => {
-      return GPX.forwardString(text);
-    });
-  }
-  forwardString(text: string) {
-    return EitherAsync<ConvertError, ConvertResult>(
-      async function forwardGpx() {
-        const tcx = await import("@tmcw/togeojson").then(
-          (module) => module.gpx
-        );
-        const dom = await toDom(text);
-        const geojson = tcx(dom);
-        return okResult(geojson as FeatureCollection);
-      }
-    );
-  }
+	id = "gpx" as const;
+	label = "GPX";
+	extensions = [".gpx"];
+	mimes = [] as string[];
+	filenames = [] as string[];
+	forwardBinary(file: ArrayBuffer, _options: ImportOptions) {
+		return readAsText(file).chain((text) => {
+			return GPX.forwardString(text);
+		});
+	}
+	forwardString(text: string) {
+		return EitherAsync<ConvertError, ConvertResult>(
+			async function forwardGpx() {
+				const tcx = await import("@tmcw/togeojson").then(
+					(module) => module.gpx,
+				);
+				const dom = await toDom(text);
+				const geojson = tcx(dom as unknown as Document);
+				return okResult(geojson as FeatureCollection);
+			},
+		);
+	}
 }
 
 export const GPX = new CGPX();
