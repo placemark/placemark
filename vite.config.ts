c4ad9e3500607cefe161a87dc3e9b4d15309044d
Tom MacWright <tom@macwright.com>
2026-07-08 13:25:02 -0400

    Remove tsconfig-paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
+import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
+
+const localRoots = ["app", "types", "state", "integrations", "test", "vendor", 'styles'];
 
 // https://vitejs.dev/config/
 export default defineConfig((env) => ({
+  resolve: {
+    alias: localRoots.map((dir) => ({
+      find: dir,
+      replacement: resolve(import.meta.dirname, dir),
+    })),
+  },
   plugins:
     env.mode === "test"
-      ? [react(), tsconfigPaths()]
-      : [react(), tsconfigPaths(), nodePolyfills()],
+      ? [react()]
+      : [react(), nodePolyfills()],
   worker: {
     format: "es",
-    plugins: () => [tsconfigPaths()],
+    plugins: () => [],
   },
   test: {
     dir: "./",
